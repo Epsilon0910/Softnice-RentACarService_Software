ab493558e18ccdc2b00a2eb3b2d1ba0a1c0714ad
Charchit Joshi <charchit.jd@gmail.com>
2017-10-24 14:07:52 +0530

    Add project files.

---
+++ RentACar/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+        public ActionResult Works()
+        {
+            return View();
+        }
+        public ActionResult Book()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Landing()
+        {
+            return View();
+        }
+    }
+}
