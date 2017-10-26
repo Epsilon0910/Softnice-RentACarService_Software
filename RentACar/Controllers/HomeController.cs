39ad2f3f068e3dbdad9ec7ca608b815c79d151f3
imadsultan <best_imad@hotmail.com>
2017-10-26 17:14:42 +0530

    Adding admin's landing page

--- RentACar/Controllers/HomeController.cs
+++ RentACar/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace RentACar.Controllers
         {
             return View();
         }
+        public ActionResult Adminlanding()
+        {
+            return View();
+        }
     }
 }
