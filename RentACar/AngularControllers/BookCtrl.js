c0182c113fd9d7feec800416fe43e625f8db6725
imadsultan <best_imad@hotmail.com>
2017-11-11 23:01:04 +0530

    acknowledge changes

--- RentACar/AngularControllers/BookCtrl.js
+++ RentACar/AngularControllers/BookCtrl.js
@@ -2,7 +2,9 @@
     $scope.cusname = '';
     $scope.cusnumber = '';
     $scope.email = '';
+    $scope.CountryList = [{ Name: 'India', ID: '1' }, { Name: 'KSA', ID: '2' }]
     $scope.CountrycodeList = [{ Name: '+1', ID: 'United States' }, { Name: '+972', ID: 'Italy' }]
+    $scope.CurrencyList=[{Name:'$', ID: '1'}, {Name:'INR', ID:'2'}]
   
     $scope.savecardetails = function () {
         $scope.myForm.submitted = true
