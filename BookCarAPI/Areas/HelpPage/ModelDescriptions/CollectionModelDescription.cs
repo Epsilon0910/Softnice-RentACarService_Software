fc0c3d40ec981569a0453f48b157f49deb1f5536
Charchit Joshi <charchit.jd@gmail.com>
2017-11-16 13:56:42 +0530

    adding BookCarAPI to project

---
+++ BookCarAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BookCarAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
