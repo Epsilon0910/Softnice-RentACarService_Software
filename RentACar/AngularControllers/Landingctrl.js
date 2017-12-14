22fa4280f742e52900072f43c0bc48811b6c090b
ravishingfaraz <ravishfaraz@gmail.com>
2017-12-14 16:22:15 +0530

    final changes after review

--- RentACar/AngularControllers/Landingctrl.js
+++ RentACar/AngularControllers/Landingctrl.js
@@ -3,8 +3,8 @@
     $scope.yearlist = [{ Name: '2005', ID: '1' }, { Name: '2006', ID: '2' }, { Name: '2007', ID: '3' }]
 
     
-    var makelist = [{ Name: 'Volkswagen', ID: '1' }, { Name: 'BMW', ID: '2' }, { Name: 'Pagani', ID: '2' }]
-    //$scope.makelist = makelist;
+    $scope.makelist = [{ Name: 'Volkswagen', ID: '1' }, { Name: 'BMW', ID: '2' }, { Name: 'Pagani', ID: '2' }]
+    
 
     $scope.Namelist = [{ Name: 'BMW Z1', ID: '1' }, { Name: 'Zonda', ID: '2' }, { Name: 'Audi R8', ID: '3' }]
 
